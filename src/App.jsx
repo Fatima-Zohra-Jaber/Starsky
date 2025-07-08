af0edcf7d33e0409a6d6f30a6bb606ae24db1fca
Fatima Zohra Jaber <jaber.fatiima.zohra@gmail.com>
2025-07-08 17:13:56 +0100

    V1

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Starsky App!</h1>
+    </div>
+  );
+}
